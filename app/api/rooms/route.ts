9b545f7a2f655e6efc9245a26aacb47d13c833ef
Rossen <jetso1127@gmail.com>
2024-09-28 12:47:15 +0800

    新增房間新增功能以及相關路由

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -1,5 +1,5 @@
 import dbConnect from '@/backend/config/dbControllers';
-import { allRooms } from '@/backend/controllers/roomControllers';
+import { allRooms, newRoom } from '@/backend/controllers/roomControllers';  // 引入 newRoom 方法
 import { createEdgeRouter } from 'next-connect';
 import { NextRequest } from 'next/server';
 
@@ -14,7 +14,13 @@ const router = createEdgeRouter<NextRequest, RequestContext>();
 dbConnect();
 
 router.get(allRooms);
+router.post(newRoom); // 新增 POST 方法，用於處理房間新增
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
+  return router.run(request, ctx);
+}
+
+// 新增 POST 方法，用於處理房間新增
+export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,7 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
+import Room from '../models/room'; // 引入 Room model
 
 export const allRooms = async (req: NextRequest) => {
   return NextResponse.json({
     data: 'Hello, World!',
   });
 };
+
+// 新增房間
+export const newRoom = async (req: NextRequest) => {
+  const body = await req.json();
+  const room = await Room.create(body);
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+}
