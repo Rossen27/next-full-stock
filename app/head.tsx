58a4040439e115ad05331d8d9eb9500f33597bca
RossenLin <97585139+Rossen27@users.noreply.github.com>
2024-10-06 17:17:05 +0800

    前端設定前準備

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+      <title>ACPGaming</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="ACPGaming" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
+
+export default Head
