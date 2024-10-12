f8497c09d80632c11a68599f57aaa022df402e96
Rossen <jetso1127@gmail.com>
2024-10-12 14:33:42 +0800

    設定全域以利之後全局的狀態或功能設定

---
+++ app/GlobalProvider.tsx
@@ -0,0 +1,19 @@
+/**
+ * GlobalProvider 是一個用來包裹其他子元件的高階元件（HOC），
+ * 它可以提供一個全局的上下文環境，讓被包裹的子元件能夠共享某些全局的狀態或功能。
+ * 這個元件本身不執行任何複雜的邏輯，只是簡單地渲染其包裹的子元件。
+ *
+ * @param {Object} props - 傳遞給元件的參數物件
+ * @param {React.ReactNode} props.children - 需要被包裹的子元件，可以是一個或多個元件。
+ * 
+ * @returns {JSX.Element} - 返回一個 JSX 元素，包含所有被包裹的子元件。
+ * 
+ * @example
+ * // 使用 GlobalProvider 包裹應用程式中的其他元件
+ * <GlobalProvider>
+ *   <App />
+ * </GlobalProvider>
+ */
+export function GlobalProvider({ children }: { children: React.ReactNode }) {
+  return <>{children}</>; // 將子元件渲染並顯示在頁面上
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import Head from "./head";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import Head from './head';
+import { GlobalProvider } from './GlobalProvider';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export default function RootLayout({
@@ -20,12 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <Head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
   );
