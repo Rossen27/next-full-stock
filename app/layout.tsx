f1f2da9c8d40bc0876abf7ca0d7bbbd27b9145f9
Rossen <jetso1127@gmail.com>
2024-10-31 10:23:05 +0800

    style: 代碼風格變更，移除沒必要的字體設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,8 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import Head from './head';
 import { GlobalProvider } from './GlobalProvider';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export default function RootLayout({
   children,
@@ -25,9 +12,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <Head />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <GlobalProvider>
           <Header />
           {children}
